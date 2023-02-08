45bd3c53c071b3e6355209c16a5f69d599b0bfd2
yazmusleh1 <32080833+yazmusleh1@users.noreply.github.com>
2023-02-08 16:40:53 -0600

    Slight refactor

--- src/components/CalendarColumn.tsx
+++ src/components/CalendarColumn.tsx
@@ -1,4 +1,3 @@
-import { Button, TextField } from '@mui/material'
 import React, { useState } from 'react'
 import Title from './Title'
 import Calendar from 'react-calendar'
@@ -8,33 +7,33 @@ type CalendarProps = {
     tasks: { id: number, title: string, date: Date, isComplete: boolean }[],
 }
 
-
 const CalendarColumn: React.FC<CalendarProps> = ({tasks}) => {
-    const [date, setDate] = useState(new Date())
+    const [selectedDate, setSelectedDate] = useState(new Date())
+    const filteredTasks = tasks.filter(task => {
+        const taskDate = new Date(task.date);
+        return (
+            taskDate.getFullYear() === selectedDate.getFullYear() &&
+            taskDate.getMonth() === selectedDate.getMonth() &&
+            taskDate.getDate() === selectedDate.getDate()
+        );
+    });
 
     return (
         <>
             <div className='border-2 col-span-1 w-1/3'>
             <Title>Calendar</Title>
                 <div className="calendar-container">
-                    <Calendar onChange={setDate} value={date}/>
+                    <Calendar onChange={setSelectedDate} value={selectedDate}/>
                 </div>
                 <div className="text-center">
-                    Selected date: {date.toDateString()}
+                    Selected date: {selectedDate.toDateString()}
                 </div>
                 <div id='tasks' className='ml-3'>
-                    {tasks.filter(task => {
-                        const taskDate = new Date(task.date);
-                        return (
-                            taskDate.getFullYear() === date.getFullYear() &&
-                            taskDate.getMonth() === date.getMonth() &&
-                            taskDate.getDate() === date.getDate()
-                        );
-                        }).map((task) => (
+                    {filteredTasks.map((task) => (
                         <TaskPreview
                             task={task}
                             index={task.id}
-                         />
+                        />
                     ))}
                 </div>
             </div>
